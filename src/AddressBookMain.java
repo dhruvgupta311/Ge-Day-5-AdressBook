700c1bcaaf8ffef759655d44e49112a4a74e2474
Dhruv <guptadhruv1908@gmail.com>
2024-12-13 22:55:41 +0530

    Implemented UC1: Ability to add contacts

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBookMain {
+    private List<Contact> addressBook;
+
+    public AddressBookMain() {
+        this.addressBook = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        addressBook.add(contact);
+        System.out.println("Contact added: " + contact);
+    }
+
+    public void displayContacts() {
+        System.out.println("Contacts in Address Book:");
+        for (Contact contact : addressBook) {
+            System.out.println(contact);
+        }
+    }
+
     public static void main(String[] args) {
+        AddressBookMain addressBookMain = new AddressBookMain();
         System.out.println("Welcome to Address Book Program");
         System.out.println("Running on Main Branch");
+
+        // Add a contact
+        Contact contact = new Contact("John", "Doe", "123 Main St", "New York", "NY", "10001", "9876543210", "john.doe@example.com");
+        addressBookMain.addContact(contact);
+
+        // Display all contacts
+        addressBookMain.displayContacts();
     }
 }
